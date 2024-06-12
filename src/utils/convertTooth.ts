9745ff8ec9222efc2af5c64b8404d5ef5b0b7669
Codiving <codivingmoon@gmail.com>
2024-06-12 12:35:00 +0900

    feat: Remove unnecessary from argument in convertTooth

--- src/utils/convertTooth.ts
+++ src/utils/convertTooth.ts
@@ -2,59 +2,46 @@ import { NotationType, Teeth } from "..";
 import { FDI_TEETH, FdiTeeth } from "../fdi";
 import { PALMER_TEETH, PalmerTeeth } from "../palmer";
 import { UNIVERSAL_TEETH, UniversalTeeth } from "../universal";
+import { isFdiTeeth, isUniversalTeeth } from "./common";
 
 export function convertTooth(
   teeth: FdiTeeth[],
-  from: "fdi",
   to: "universal"
 ): UniversalTeeth[];
 
-export function convertTooth(
-  teeth: FdiTeeth[],
-  from: "fdi",
-  to: "palmer"
-): PalmerTeeth[];
+export function convertTooth(teeth: FdiTeeth[], to: "palmer"): PalmerTeeth[];
+
+export function convertTooth(teeth: UniversalTeeth[], to: "fdi"): FdiTeeth[];
 
 export function convertTooth(
   teeth: UniversalTeeth[],
-  from: "universal",
-  to: "fdi"
-): FdiTeeth[];
-
-export function convertTooth(
-  teeth: UniversalTeeth[],
-  from: "universal",
   to: "palmer"
 ): PalmerTeeth[];
 
-export function convertTooth(
-  teeth: PalmerTeeth[],
-  from: "palmer",
-  to: "fdi"
-): FdiTeeth[];
+export function convertTooth(teeth: PalmerTeeth[], to: "fdi"): FdiTeeth[];
 
 export function convertTooth(
   teeth: PalmerTeeth[],
-  from: "palmer",
   to: "universal"
 ): UniversalTeeth[];
 
 export function convertTooth(
   teeth: FdiTeeth[] | UniversalTeeth[] | PalmerTeeth[],
-  from: NotationType,
   to: NotationType
 ): FdiTeeth[] | UniversalTeeth[] | PalmerTeeth[] {
   if (!teeth.length) return [];
 
-  const toTeeth = (to === "fdi"
+  const tooth = teeth[0];
+
+  const fromTeeth = (isFdiTeeth(tooth)
     ? FDI_TEETH
-    : to === "universal"
+    : isUniversalTeeth(tooth)
     ? UNIVERSAL_TEETH
     : PALMER_TEETH) as unknown as Teeth[];
 
-  const fromTeeth = (from === "fdi"
+  const toTeeth = (to === "fdi"
     ? FDI_TEETH
-    : from === "universal"
+    : to === "universal"
     ? UNIVERSAL_TEETH
     : PALMER_TEETH) as unknown as Teeth[];
 
