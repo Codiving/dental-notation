379e1c5025419c7f610f5b9a216b71347aeca294
Codiving <codivingmoon@gmail.com>
2024-06-18 21:54:28 +0900

    chore: Export variables related to fdi and type

--- src/fdi.ts
+++ src/fdi.ts
@@ -4,7 +4,7 @@ export const FDI_PRIMARY_TEETH_UPPER_LEFT = [
 export const FDI_PRIMARY_TEETH_UPPER_RIGHT = [
   21, 22, 23, 24, 25, 26, 27, 28
 ] as const;
-const FDI_PRIMARY_TEETH_UPPER = [
+export const FDI_PRIMARY_TEETH_UPPER = [
   ...FDI_PRIMARY_TEETH_UPPER_LEFT,
   ...FDI_PRIMARY_TEETH_UPPER_RIGHT
 ] as const;
@@ -13,6 +13,7 @@ export type FdiPrimaryTeethUpperLeft =
   (typeof FDI_PRIMARY_TEETH_UPPER_LEFT)[number];
 export type FdiPrimaryTeethUpperRight =
   (typeof FDI_PRIMARY_TEETH_UPPER_RIGHT)[number];
+export type FdiPrimaryTeethUpper = (typeof FDI_PRIMARY_TEETH_UPPER)[number];
 
 export const FDI_PRIMARY_TEETH_LOWER_LEFT = [
   48, 47, 46, 45, 44, 43, 42, 41
@@ -20,7 +21,7 @@ export const FDI_PRIMARY_TEETH_LOWER_LEFT = [
 export const FDI_PRIMARY_TEETH_LOWER_RIGHT = [
   31, 32, 33, 34, 35, 36, 37, 38
 ] as const;
-const FDI_PRIMARY_TEETH_LOWER = [
+export const FDI_PRIMARY_TEETH_LOWER = [
   ...FDI_PRIMARY_TEETH_LOWER_LEFT,
   ...FDI_PRIMARY_TEETH_LOWER_RIGHT
 ] as const;
@@ -29,6 +30,7 @@ export type FdiPrimaryTeethLowerLeft =
   (typeof FDI_PRIMARY_TEETH_LOWER_LEFT)[number];
 export type FdiPrimaryTeethLowerRight =
   (typeof FDI_PRIMARY_TEETH_LOWER_RIGHT)[number];
+export type FdiPrimaryTeethLower = (typeof FDI_PRIMARY_TEETH_LOWER)[number];
 
 export const FDI_PRIMARY_TEETH = [
   ...FDI_PRIMARY_TEETH_UPPER,
@@ -36,24 +38,33 @@ export const FDI_PRIMARY_TEETH = [
 ] as const;
 export type FdiPrimaryTeeth = (typeof FDI_PRIMARY_TEETH)[number];
 
-const FDI_BABY_TEETH_UPPER_LEFT = [55, 54, 53, 52, 51] as const;
-const FDI_BABY_TEETH_UPPER_RIGHT = [61, 62, 63, 64, 65] as const;
-const FDI_BABY_TEETH_UPPER = [
+export const FDI_BABY_TEETH_UPPER_LEFT = [55, 54, 53, 52, 51] as const;
+export const FDI_BABY_TEETH_UPPER_RIGHT = [61, 62, 63, 64, 65] as const;
+export const FDI_BABY_TEETH_UPPER = [
   ...FDI_BABY_TEETH_UPPER_LEFT,
   ...FDI_BABY_TEETH_UPPER_RIGHT
 ] as const;
+export type FdiBabyTeethUpperLeft = (typeof FDI_BABY_TEETH_UPPER_LEFT)[number];
+export type FdiBabyTeethUpperRight =
+  (typeof FDI_BABY_TEETH_UPPER_RIGHT)[number];
+export type FdiBabyTeethUpper = (typeof FDI_BABY_TEETH_UPPER)[number];
 
-const FDI_BABY_TEETH_LOWER_LEFT = [85, 84, 83, 82, 81] as const;
-const FDI_BABY_TEETH_LOWER_RIGHT = [71, 72, 73, 74, 75] as const;
-const FDI_BABY_TEETH_LOWER = [
+export const FDI_BABY_TEETH_LOWER_LEFT = [85, 84, 83, 82, 81] as const;
+export const FDI_BABY_TEETH_LOWER_RIGHT = [71, 72, 73, 74, 75] as const;
+export const FDI_BABY_TEETH_LOWER = [
   ...FDI_BABY_TEETH_LOWER_LEFT,
   ...FDI_BABY_TEETH_LOWER_RIGHT
 ] as const;
+export type FdiBabyTeethLowerLeft = (typeof FDI_BABY_TEETH_LOWER_LEFT)[number];
+export type FdiBabyTeethLowerRight =
+  (typeof FDI_BABY_TEETH_LOWER_RIGHT)[number];
+export type FdiBabyTeethLower = (typeof FDI_BABY_TEETH_LOWER)[number];
 
-const FDI_BABY_TEETH = [
+export const FDI_BABY_TEETH = [
   ...FDI_BABY_TEETH_UPPER,
   ...FDI_BABY_TEETH_LOWER
 ] as const;
+export type FdiBabyTeeth = (typeof FDI_BABY_TEETH)[number];
 
 export const FDI_TEETH = [...FDI_PRIMARY_TEETH, ...FDI_BABY_TEETH] as const;
 export type FdiTeeth = (typeof FDI_TEETH)[number];
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,35 @@
-import { FdiTeeth } from "./fdi";
+import {
+  FDI_BABY_TEETH,
+  FDI_BABY_TEETH_LOWER,
+  FDI_BABY_TEETH_LOWER_LEFT,
+  FDI_BABY_TEETH_LOWER_RIGHT,
+  FDI_BABY_TEETH_UPPER,
+  FDI_BABY_TEETH_UPPER_LEFT,
+  FDI_BABY_TEETH_UPPER_RIGHT,
+  FDI_PRIMARY_TEETH,
+  FDI_PRIMARY_TEETH_LOWER,
+  FDI_PRIMARY_TEETH_LOWER_LEFT,
+  FDI_PRIMARY_TEETH_LOWER_RIGHT,
+  FDI_PRIMARY_TEETH_UPPER,
+  FDI_PRIMARY_TEETH_UPPER_LEFT,
+  FDI_PRIMARY_TEETH_UPPER_RIGHT,
+  FDI_TEETH,
+  FdiBabyTeeth,
+  FdiBabyTeethLower,
+  FdiBabyTeethLowerLeft,
+  FdiBabyTeethLowerRight,
+  FdiBabyTeethUpper,
+  FdiBabyTeethUpperLeft,
+  FdiBabyTeethUpperRight,
+  FdiPrimaryTeeth,
+  FdiPrimaryTeethLower,
+  FdiPrimaryTeethLowerLeft,
+  FdiPrimaryTeethLowerRight,
+  FdiPrimaryTeethUpper,
+  FdiPrimaryTeethUpperLeft,
+  FdiPrimaryTeethUpperRight,
+  FdiTeeth
+} from "./fdi";
 import { PalmerTeeth } from "./palmer";
 import { UniversalTeeth } from "./universal";
 import { convertTeeth, getTeethString } from "./utils";
@@ -7,6 +38,44 @@ type Teeth = FdiTeeth | UniversalTeeth | PalmerTeeth;
 
 type NotationType = "fdi" | "universal" | "palmer";
 
-export { convertTeeth, getTeethString };
+export {
+  FDI_BABY_TEETH,
+  FDI_BABY_TEETH_LOWER,
+  FDI_BABY_TEETH_LOWER_LEFT,
+  FDI_BABY_TEETH_LOWER_RIGHT,
+  FDI_BABY_TEETH_UPPER,
+  FDI_BABY_TEETH_UPPER_LEFT,
+  FDI_BABY_TEETH_UPPER_RIGHT,
+  FDI_PRIMARY_TEETH,
+  FDI_PRIMARY_TEETH_LOWER,
+  FDI_PRIMARY_TEETH_LOWER_LEFT,
+  FDI_PRIMARY_TEETH_LOWER_RIGHT,
+  FDI_PRIMARY_TEETH_UPPER,
+  FDI_PRIMARY_TEETH_UPPER_LEFT,
+  FDI_PRIMARY_TEETH_UPPER_RIGHT,
+  FDI_TEETH,
+  convertTeeth,
+  getTeethString
+};
 
-export type { FdiTeeth, NotationType, PalmerTeeth, Teeth, UniversalTeeth };
+export type {
+  FdiBabyTeeth,
+  FdiBabyTeethLower,
+  FdiBabyTeethLowerLeft,
+  FdiBabyTeethLowerRight,
+  FdiBabyTeethUpper,
+  FdiBabyTeethUpperLeft,
+  FdiBabyTeethUpperRight,
+  FdiPrimaryTeeth,
+  FdiPrimaryTeethLower,
+  FdiPrimaryTeethLowerLeft,
+  FdiPrimaryTeethLowerRight,
+  FdiPrimaryTeethUpper,
+  FdiPrimaryTeethUpperLeft,
+  FdiPrimaryTeethUpperRight,
+  FdiTeeth,
+  NotationType,
+  PalmerTeeth,
+  Teeth,
+  UniversalTeeth
+};
